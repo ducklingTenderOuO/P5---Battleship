7eaf38ffcc7eef734a5ab03bd49591b97e12a1c7
Astrid <astrid.jimenez.barrera@uabc.edu.mx>
2025-05-22 19:04:25 -0700

    Update Barco.java

--- Barco.java
+++ Barco.java
@@ -1,33 +1,46 @@
-public class Barco {
+/**
+ * Representa un barco en el juego, con su orientación, tamaño
+ * y posición en el tablero.
+ *
+ * @author (AYJB)
+ * @version (ABR 2025)
+ */
+
+import java.io.Serializable;
+
+public class Barco implements Serializable {
+    private static final long serialVersionUID = 1L;
     private String orientacion;
     private int tamaño;
     private int capa;
     private int fila;
     private int columna;
+    private String[] dibujo;
 
     public Barco(String orientacion, int tamaño, int capa, int fila, int columna) {
-        String[] barco = new String[🚢][tamaño];
-        this.orientacion = "";
+        this.orientacion = orientacion;
         this.tamaño = tamaño;
         this.capa = capa;
         this.fila = fila;
         this.columna = columna;
+        this.dibujo = new String[tamaño];
+        for (int i = 0; i < tamaño; i++) {
+            dibujo[i] = "🚢";
+        }
     }
 
-    public String getOrientacion() {return orientacion;}
-    public void setOrientacion(String orientacion) {this.orientacion = orientacion;}
-
-    public int getCapa() {return capa;}
-    public void setCapa(int capa) {this.capa = capa;}
-
-    public int getFila() {return fila;}
-    public void setFila(int fila) {this.fila = fila;}
-
-    public int getColumna() {return columna;}
-    public void setColumna(int columna) {this.columna = columna;}
-
-    public boolean sePuedeColocarBarco(int capaDestino, int filaDestino, int columnaDestino) {
-        
+    public void mostrarBarco() {
+        System.out.print("Barco (" + tamaño + "): ");
+        for (String parte : dibujo) {
+            System.out.print(parte + " ");
+        }
+        System.out.println();
     }
 
+    // Getteeeeeeeeeeeeeeeers
+    public String getOrientacion() { return orientacion; }
+    public int getTamaño() { return tamaño; }
+    public int getCapa() { return capa; }
+    public int getFila() { return fila; }
+    public int getColumna() { return columna; }
 }
